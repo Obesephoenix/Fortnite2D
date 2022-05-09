27e94576e890c0039688a4bcc0981241772748a3
Obesephoenix <john830beck@gmail.com>
2022-05-09 08:11:38 +0100

    Initial commit

--- entity.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass, field
-from object import Object
-
-
-@dataclass
-class Entity:
-    obj: Object
--- main.py
+++ main.py
@@ -1,31 +1,12 @@
-import pygame
-from pygame import Vector2
-from util.constants import *
-from util.mouse import render_crosshair
-import object as obj
+import pygame as pg
 
-pygame.init()
+pg.init()
 
-screen = pygame.display.set_mode(SCREEN_SIZE, pygame.SCALED + pygame.RESIZABLE)
-display = pygame.Surface(DISPLAY_SIZE)
-clock = pygame.time.Clock()
-
-pygame.mouse.set_visible(False)
-pygame.display.set_caption("Fortnite 2D")
-
-test_object = obj.Object(pygame.Surface((25, 25)), Vector2(150, 100))
+screen = pg.display.set_mode((900, 450))
+display = pg.Surface((900, 450))
 
 while True:
     display.fill((255, 255, 255))
-    render_crosshair(display, None)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-
-    obj.update_object(test_object)
-    obj.render_object(display, test_object)
-
-    screen.blit(pygame.transform.scale(display, screen.get_size()), (0, 0))
-    pygame.display.update()
-    clock.tick(FPS)
+    screen.blit(display, (0, 0))
+    pg.display.update()
